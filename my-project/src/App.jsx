6e2d215d1681f85d0631b23f40ca7c5059a531de
Duc Anh Nguyen Tho <ducanh.nguyentho@petronas.com>
2023-01-18 15:20:53 +0700

    init

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Gallery } from './page/Gallery'
+import { Tabs } from './page/Tabs'
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
